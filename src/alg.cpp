50ed46afcf7efe5752a7bdffca6a31c43e4c26f4
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-02-15 18:43:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,65 +3,64 @@
 #include "alg.h"
 #include <cmath>
 
-
 bool checkPrime(uint64_t value) {
-  for (int i = 2; i <= sqrt(value); i++)
-	{
-		if (value % i == 0) {
-			return false;
-		}
-		else {
-			continue;
-		}
-	}
-	if (value == 0 || value == 1) {
-		return false;
-	}
-	if (value == 2) {
-		return true;
-	}
-	return true;
+for (int i = 2; i <= sqrt(value); i++)
+{
+if (value % i == 0) {
+return false;
+}
+else {
+continue;
+}
+}
+if (value == 0 || value == 1) {
+return false;
+}
+if (value == 2) {
+return true;
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 1) {
-		return 2;
-	}
-	else if (n <= 0) {
-		return false;
-	}
-	int k = 1;
-	for (int i = 4; ;i++ ) {
-		if (checkPrime(i)) {
-			k = k + 1;
-			if (k == n) {
-				return i;
-			}
-		}
-	}
+if (n == 1) {
+return 2;
+}
+else if (n <= 0) {
+return false;
+}
+int k = 1;
+for (int i = 4; ;i++ ) {
+if (checkPrime(i)) {
+k = k + 1;
+if (k == n) {
+return i;
+}
+}
+}
 }
 
 uint64_t nextPrime(uint64_t value) {
-  if (value <= 0) {
-		return 1;
-	}
-	while (true) {
-		if (checkPrime(++value)) {
-			return value;
-		}
-		else {
-			continue;
-		}
-	}
+if (value <= 0) {
+return 1;
+}
+while (true) {
+if (checkPrime(++value)) {
+return value;
+}
+else {
+continue;
+}
+}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
-	for (int i = 0; i < hbound; i++)
-	{
-		if (checkPrime(i)) {
-			sum += i;
-		}
-	}
-	return sum;
+int sum = 0;
+for (int i = 0; i < hbound; i++)
+{
+if (checkPrime(i)) {
+sum += i;
+}
+}
+return sum;
 }
