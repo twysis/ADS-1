c512c2bc79964d71e26d64dd5f3d653ff393fbd9
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-02-17 13:00:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,56 +6,56 @@
 
 bool checkPrime(uint64_t value) {
 for (int i = 2; i <= sqrt(value); i++) {
-if (value % i == 0) {
-return false;
-} else {
-continue;
+  if (value % i == 0) {
+    return false;
+  } else {
+    continue;
+  }
 }
-}
-if (value == 0 || value == 1) {
-return false;
-}
-if (value == 2) {
-return true;
-}
-return true;
+  if (value == 0 || value == 1) {
+    return false;
+  }
+  if (value == 2) {
+    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-if (n == 1) {
-return 2;
-} else if (n <= 0) {
-return false;
-}
-uint64_t k = 1;
-for (int i = 3;  ; i++) {
-if (checkPrime(i)) {
-k = k + 1;
-if (k == n) {
-return i;
-}
-}
-}
+  if (n == 1) {
+    return 2;
+  } else if (n <= 0) {
+    return false;
+  }
+  uint64_t k = 1;
+  for (int i = 3;  ; i++) {
+    if (checkPrime(i)) {
+      k = k + 1;
+      if (k == n) {
+        return i;
+      }
+    }
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-if (value <= 0) {
-return 1; }
-while (true) {
-if (checkPrime(++value)) {
-return value;
-} else {
-continue;
-}
-}
+  if (value <= 0) {
+    return 1; }
+  while (true) {
+    if (checkPrime(++value)) {
+      return value;
+    } else {
+      continue;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t sum = 0;
-for (int i = 0; i < hbound; i++) {
-if (checkPrime(i)) {
-sum += i;
-}
-}
-return sum;
+  uint64_t sum = 0;
+  for (int i = 0; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
